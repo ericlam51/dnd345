0e0ffb7b2bd98001c2cd4a32c28e4d44e86046f3
Carson Huang <carsonhuang2010@gmail.com>
2016-11-10 14:05:42 -0500

    Clean main.cpp

--- DnD/src/main/Main.cpp
+++ DnD/src/main/Main.cpp
@@ -1,14 +1,7 @@
-#include <iostream>
-#include <ctime>
-#include <math.h>
-#include <string>
-#include "view\interactable\godmode\GodmodeInteractableView.h"
-#include "view\map\godmode\GodmodeMapView.h"
 using namespace std;
 
 int main()
 {
-	//GodmodeInteractableView::interactableFileSelectionView();
-	GodmodeMapView::fileOptionsMenuView();
+
 	return 0;
 }
