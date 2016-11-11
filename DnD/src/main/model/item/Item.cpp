0bfb9a45a733c10aca373f86629fcdc47958fb9b
Roberto Ruffolo <robertoruffolo@hotmail.com>
2016-11-10 21:15:17 -0500

    Added documentation

--- DnD/src/main/model/item/Item.cpp
+++ DnD/src/main/model/item/Item.cpp
@@ -1,7 +1,9 @@
 #include "Item.h"
 
 
-
+//! constructor that initializes the item type and the name of the item if the item type is valid
+//! @param itemType: the type of item to be created
+//! @param name: the name of the item
 Item::Item(string itemType, string name):type(itemType)
 {
 	if(validItem(itemType))
@@ -19,16 +21,22 @@ Item::~Item()
 {
 }
 
+//! getter method
+//! @return the name of the item
 string Item::getItemName()
 {
 	return itemName;
 }
 
+//! setter method
 void Item::setItemName(string name)
 {
 	itemName = name;
 }
 
+//! check if an item has a valid type
+//! @param type: type of the item
+//! @return bool
 bool Item::validItem(string type)
 {
 	for (int i = 0; i <= HELMET; i++)
--- DnD/src/main/model/item/ItemContainer.cpp
+++ DnD/src/main/model/item/ItemContainer.cpp
@@ -1,7 +1,8 @@
 #include "ItemContainer.h"
 
 
-
+//! default constructor that initializes the item container with a map<string, vector> preset with 
+//! an empty vector for each item type
 ItemContainer::ItemContainer()
 {
 	//iterate through all item types defined in ItemType enum
@@ -17,13 +18,16 @@ ItemContainer::~ItemContainer()
 {
 }
 
-
+//! add an item to the appropriate vector
+//! @param item: the item to be added
 void ItemContainer::addItem(Item* item)
 {
 
 	items[item->type].push_back(item);
 }
 
+//! remove an item to the appropriate vector
+//! @param: the item to be removed
 void ItemContainer::removeItem(Item* item)
 {
 	for (vector<Item*>::iterator it = items[item->type].begin(); it != items[item->type].end(); ++it)
@@ -36,6 +40,10 @@ void ItemContainer::removeItem(Item* item)
 	}
 }
 
+//! get an item
+//! @param type: the type of the item to be returned
+//! @param index: the index of the item in its appropriate vector
+//! @return the item requested
 Item* ItemContainer::getItem(string type, int index)
 {
 	for (vector<Item*>::iterator it = items[type].begin(); it != items[type].end(); ++it)
@@ -49,6 +57,9 @@ Item* ItemContainer::getItem(string type, int index)
 	return NULL;
 }
 
+//! get the vector according to a item type
+//! @param type: the type to be retrieved
+//! @return the vector of items of a specific type
 vector<Item*> ItemContainer::getItemsOfType(string type)
 {
 	return items[type];
