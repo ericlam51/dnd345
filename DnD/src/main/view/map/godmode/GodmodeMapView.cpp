2f9afda6e69ea9aa2b4fa9fcb8482c34a4ee8b8f
Carson Huang <carsonhuang2010@gmail.com>
2016-11-25 21:44:04 -0500

    Map: add option to save/load multiple files

--- DnD/src/main/view/map/godmode/GodmodeMapView.cpp
+++ DnD/src/main/view/map/godmode/GodmodeMapView.cpp
@@ -50,6 +50,25 @@ void GodmodeMapView::mapCreationInputView() {
 	GodmodeMapController::instance()->newMap(width, height);
 }
 
+void GodmodeMapView::mapChooseSaveMapFileView(vector<string> filenames) {
+	cout << "Please select one of the following files:" << endl;
+
+	for (int i = 0; i < filenames.size(); i++) {
+		cout << i+1 << ". " << filenames[i] << endl;
+	}
+
+	cout << "Selection: ";
+
+	int input;
+
+	cin >> input;
+
+	cin.clear(); //if cin fails because of wrong data type, clear error flag
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');  //clears the cin buffer
+
+	GodmodeMapController::instance()->loadMap(input-1);
+}
+
 //! Show options to edit the map
 void GodmodeMapView::mapOptionsMenuView() {
 	int input;
@@ -111,6 +130,15 @@ void GodmodeMapView::mapFillOptionsMenuView() {
 		break;
 	default: cout << "Invalid input"; break;
 	}
+}
+
+void GodmodeMapView::mapAskSaveFileName() {
+	cout << "Please enter filename: ";
+	string filename;
+
+	cin >> filename;
+
+	GodmodeMapController::instance()->saveMap(filename);
 }
 
 /* WARNING MESSAGES */
